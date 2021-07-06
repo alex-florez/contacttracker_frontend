63ff925c6a85c45fd345f77077498685f9cda102
Alex Florez <UO264371@uniovi.es>
2021-07-06 13:39:09 +0200

    Añadiendo comprobación de punto, solo se permiten números enteros

--- src/components/config/NotifyConfig.vue
+++ src/components/config/NotifyConfig.vue
@@ -97,7 +97,9 @@ export default {
          */
         isNumber(event) {
             var charCode = (event.which) ? event.which : event.keyCode
-            if(charCode > 31 && (charCode < 48 || charCode > 57) && charCode !== 46){
+            if(event.key === "."){
+                event.preventDefault()
+            } else if(charCode > 31 && (charCode < 48 || charCode > 57) && charCode !== 46){
                 event.preventDefault()
             } else if(parseInt(charCode) >= 0){
                 return true
